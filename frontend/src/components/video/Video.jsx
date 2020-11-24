6df6e26c68db27ffdafa41fc247f1fdd63f0658a
rkina <rkina7@gmail.com>
2020-11-24 10:43:30 +0300

    subj

---
+++ frontend/src/components/video/Video.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+import src from './test.mp4';
+
+
+
+const Video = () => {
+    const videoSrc = src;
+    return (
+        <Container>
+        </Container>
+    );
+}
+
+export default Video;
