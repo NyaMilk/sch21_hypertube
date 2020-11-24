adf6ae86e14b2de3eb34982eb331cd0cb7dff871
rkina <rkina7@gmail.com>
2020-11-24 20:59:21 +0300

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+case "$1" in
+    "packages")
+    cd api
+    npm i
+    cd ../frontend
+    npm i
+    ;;
+    "psql")
+    echo "Ok"
+    ;;
+    "run")
+    cd api
+    npm run dev
+    ;;
+    "api")
+    cd api
+    npm run server
+    ;;
+    "front")
+    cd api
+    npm run client
+    ;;
+    *)
+    echo "Usage:"
+    echo "sh deploy.sh packages - install npm packages for node js and react"
+    echo "sh deploy.sh psql - deploy database"
+    echo "sh deploy.sh run - run servers"
+    echo "sh deploy.sh api - run api"
+    echo "sh deploy.sh front - run front"
+esac
