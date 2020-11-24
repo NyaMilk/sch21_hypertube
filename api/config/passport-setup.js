cf2c1b47c0a250032baf60a768b4cff2bf4ed71d
rkina <rkina7@gmail.com>
2020-11-24 21:54:58 +0300

    restore

--- api/config/passport-setup.js
+++ api/config/passport-setup.js
@@ -1,6 +1,8 @@
 const passport = require("passport");
 const keys = require("./keys");
 const FacebookStrategy = require('passport-facebook').Strategy;
+const GithubStrategy = require('passport-github').Strategy;
+const SchoolStrategy = require('passport-42').Strategy;
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -17,7 +19,35 @@ passport.use(new FacebookStrategy({
 },
   async (token, tokenSecret, profile, done) => {
 
-    console.log(profile);
+    // console.log(profile);
+    // const currentUser = profile._json.id_str;
+
+    done(null, profile);
+  }
+));
+
+passport.use(new GithubStrategy({
+  clientID: keys.GITHUB_ACCESS_TOKEN,
+  clientSecret: keys.GITHUB_TOKEN_SECRET,
+  callbackURL: "/api/login/github/redirect"
+},
+  async (token, tokenSecret, profile, done) => {
+
+    // console.log(profile);
+    // const currentUser = profile._json.id_str;
+
+    done(null, profile);
+  }
+));
+
+passport.use(new SchoolStrategy({
+  clientID: keys.SCHOOL_ACCESS_TOKEN,
+  clientSecret: keys.SCHOOL_TOKEN_SECRET,
+  callbackURL: "/api/login/intra/redirect"
+},
+  async (token, tokenSecret, profile, done) => {
+
+    // console.log(profile);
     // const currentUser = profile._json.id_str;
 
     done(null, profile);
