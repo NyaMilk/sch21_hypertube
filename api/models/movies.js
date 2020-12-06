ce0cd227a568a46239b0df00e1e7ef43e49bff9a
rkina <rkina7@gmail.com>
2020-12-06 22:30:48 +0300

    bef merge

--- api/models/movies.js
+++ api/models/movies.js
@@ -26,3 +26,20 @@ exports.getMovies = () => {
 
     return db.any(sql);
 }
+
+exports.getCountCards = (genres, sqlFilter) => {
+    const sql =
+        `SELECT * FROM Movies
+    WHERE ${sqlFilter}`;
+
+    return db.any(sql, [genres]);
+}
+
+exports.getCards = (genres, limit, sqlSort, sqlFilter) => {
+    const sql =
+        `SELECT title, imdb, EXTRACT(YEAR FROM dateRelease) AS year, poster, genres FROM Movies
+    WHERE ${sqlFilter} ORDER BY ${sqlSort}
+    LIMIT 9 OFFSET ($2 - 9)`;
+
+    return db.any(sql, [genres, limit]);
+}
---
+++ api/routes/movies.routes.js
@@ -0,0 +1,91 @@
+const router = require('express').Router();
+const { getCountCards, getCards } = require('../models/movies');
+
+router.post('/catalog/count', async (req, res) => {
+    try {
+        const { rateFrom, rateTo, yearFrom, yearTo, genres } = req.body;
+
+        // тут проверку на A > B?
+        // sqlFilter = `imdb >= ${rateFrom} AND imdb <= ${rateTo} AND EXTRACT(YEAR FROM year) BETWEEN ${yearFrom} AND ${yearTo} `;
+        let sqlFilter = `EXTRACT(YEAR FROM dateRelease) BETWEEN ${yearFrom} AND ${yearTo} `;
+        if (genres.length > 0)
+            sqlFilter += `AND genres && $1`;
+
+        getCountCards(genres, sqlFilter)
+            .then(data => {
+                if (data.length > 0) {
+                    res.status(200).json({
+                        result: data.length,
+                        message: "Ok",
+                        success: true
+                    });
+                }
+                else
+                    res.status(200).json({
+                        message: "No films",
+                        success: false
+                    })
+            })
+            .catch(() => {
+                res.status(200).json({
+                    message: "Ooops! Cannot find films. Try again",
+                    success: false
+                })
+            })
+    } catch (e) {
+        res.status(200).json({
+            message: "Ooops! Cannot find films. Try again",
+            success: false
+        })
+    }
+})
+
+router.post('/catalog/page', async (req, res) => {
+    try {
+        
+        const { page, sort, rateFrom, rateTo, yearFrom, yearTo, genres } = req.body;
+        let sqlSort = '',
+            limit = page * 9;
+
+        if (sort === 'yearAsc' || sort === 'yearDesc')
+            sqlSort = (sort === 'yearAsc') ? 'year ASC' : 'year DESC';
+        // sqlSort = (sort === 'yearAsc') ? 'year ASC, rate DESC' : 'year DESC, rate DESC';
+        // else if (sort === 'rateAsc' || sort === 'rateDesc')
+        //     sqlSort = (sort === 'rateAsc') ? 'rate ASC, year ASC' : 'rate DESC, year ASC';
+
+        // тут проверку на A > B?
+        // let sqlFilter = `AND imdb >= ${rateFrom} AND imdb <= ${rateTo} AND year >= ${yearFrom} AND year <= ${yearTo} `;
+        let sqlFilter = `EXTRACT(YEAR FROM dateRelease) BETWEEN ${yearFrom} AND ${yearTo} `;
+        if (genres.length > 0)
+            sqlFilter += `AND genres && $1`;
+
+        getCards(genres, limit, sqlSort, sqlFilter)
+            .then(data => {
+                if (data.length > 0) {
+                    res.status(200).json({
+                        result: data,
+                        message: "Ok",
+                        success: true
+                    });
+                }
+                else
+                    res.status(200).json({
+                        message: "No film",
+                        success: false
+                    })
+            })
+            .catch(() => {
+                res.status(200).json({
+                    message: "Ooops! Cannot find film. Try again",
+                    success: false
+                })
+            })
+    } catch (e) {
+        res.status(200).json({
+            message: "Ooops! Cannot find film. Try again",
+            success: false
+        })
+    }
+})
+
+module.exports = router;
