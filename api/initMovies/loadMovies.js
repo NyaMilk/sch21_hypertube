f7f0841225d0c362d464000f8dbf3438a50cb085
Rkina <rkina7@gmail.com>
2020-12-09 20:29:41 +0300

    fix loadmovies

--- api/initMovies/loadMovies.js
+++ api/initMovies/loadMovies.js
@@ -7,15 +7,23 @@ const getPopcornMovies = async () => {
     let raw = [];
 
     console.log('*Load popcorn movies*');
-    for (let i = 1; i <= 1; i++) {
-        const res = await axios(`https://cors-anywhere.herokuapp.com/movies-v2.api-fetch.sh/movies/${i}`, { headers: { 'X-Requested-With': true } })
-        raw.push(...res.data);
+    for (let i = 1; i <= 10; i++) {
+        try {
+            const res = await axios(`https://cors-anywhere.herokuapp.com/movies-v2.api-fetch.sh/movies/${i}`, { headers: { 'X-Requested-With': true } });
+            raw.push(...res.data);
+        } catch (e) {
+            continue
+        }
     }
 
     let movies = [];
     movies = raw.map((movie, item) => {
         let formatMovie = new Object;
         const { imdb_id, title, synopsis, genres, torrents } = movie;
+
+        if (!imdb_id || !title || !synopsis || !genres || !torrents)
+            return null
+
         formatMovie.imdb = imdb_id;
         formatMovie.enTitle = title;
         formatMovie.rate = 0;
@@ -39,15 +47,23 @@ const getYtsMovies = async () => {
     let raw = [];
 
     console.log('*Load YTS movies*');
-    for (let i = 1; i <= 1; i++) {
-        const res = await axios(`https://yts.lt/api/v2/list_movies.json?limit=50&page=${i}`)
-        raw.push(...res.data.data.movies);
+    for (let i = 1; i <= 10; i++) {
+        try {
+            const res = await axios(`https://yts.lt/api/v2/list_movies.json?limit=50&page=${i}`)
+            raw.push(...res.data.data.movies);
+        } catch (e) {
+            continue
+        }
     }
 
     let movies = [];
     movies = raw.map((movie, item) => {
         let formatMovie = new Object;
         const { imdb_code, title, synopsis, genres, torrents } = movie;
+
+        if (!imdb_code || !title || !synopsis || !genres || !torrents)
+            return null
+
         formatMovie.imdb = imdb_code;
         formatMovie.enTitle = title;
         formatMovie.rate = 0;
@@ -65,7 +81,7 @@ const getYtsMovies = async () => {
 
         return (formatMovie.torrents.length > 0) ? formatMovie : null;
     })
-    
+
     console.log('*Got YTS movies*');
     return movies;
 }
@@ -74,6 +90,10 @@ const filterMovies = async () => {
     let pops = await getPopcornMovies();
     let yts = await getYtsMovies();
     let raw = pops.concat(yts);
+
+    raw = raw.filter((el) => {
+        return el != null;
+    });
 
     for (i = 0; i < raw.length; i++) {
         for (j = 0; j < raw.length; j++) {
@@ -86,11 +106,17 @@ const filterMovies = async () => {
     let movies = [];
     movies = await Promise.all(raw.map(async (movie) => {
         try {
+            if (!movie.imdb)
+                return null;
+                
             const res = await axios(`https://api.themoviedb.org/3/movie/${movie.imdb}?api_key=${API_KEY}&language=ru`);
             const enRes = await axios(`https://api.themoviedb.org/3/movie/${movie.imdb}?api_key=${API_KEY}&language=en`);
             const en_poster_path = enRes.data.poster_path;
             const { title, genres, overview, release_date, runtime, poster_path } = res.data;
-            
+
+            if (!title || !genres || !overview || !release_date || !runtime || !poster_path)
+                return null;
+
             movie.ruTitle = title;
             movie.ruDescription = overview;
             movie.year = release_date;
@@ -142,7 +168,7 @@ const scrap = async () => {
 const addToDB = async () => {
     try {
         const final = await scrap();
-        
+
         insertMovies(final)
             .then(() => console.log('Film added to db'))
             .catch(e => console.log(e.message))
