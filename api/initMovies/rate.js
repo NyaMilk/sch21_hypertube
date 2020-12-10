359e7c659352c59e318015cf473367e5d690c088
rkina <rkina7@gmail.com>
2020-12-10 22:57:54 +0300

    rate

--- api/initMovies/rate.js
+++ api/initMovies/rate.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const file = fs.readFileSync('./data/title.ratings.tsv', "utf8");
+const file2 = fs.readFileSync('./data/iso.csv', "utf8");
 
 const tsvJSON = (tsv) => {
   const lines = tsv.split('\n');
@@ -13,7 +14,21 @@ const tsvJSON = (tsv) => {
   });
 }
 
+const csvJSON = (csv) => {
+  const lines = csv.split('\r\n');
+  const headers = lines.shift().split(',');
+  console.log(headers);
+  return lines.map(line => {
+    const data = line.split(',');
+    return headers.reduce((obj, nextKey, index) => {
+      obj[nextKey] = data[index];
+      return obj;
+    }, {});
+  });
+}
+
 const tsv = tsvJSON(file);
+const csv = csvJSON(file2);
 
 exports.getRate = (imdb) => {
   const res = tsv.filter((item) => {
@@ -22,5 +37,16 @@ exports.getRate = (imdb) => {
 
   if (res.length > 0)
     return res[0].averageRating
+  return null;
+}
+
+exports.getRuIso = (iso) => {
+  const res = csv.filter((item) => {
+    return item['code'] === iso;
+  })
+
+  if (res.length > 0) {
+    return res[0]['﻿country'];
+  }
   return null;
 }
